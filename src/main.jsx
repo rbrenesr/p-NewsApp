6576580cde360d644fb3303697a80cd9c60ce463
Rafael Brenes <rbrenesr@gmail.com>
2023-02-09 00:53:34 -0600

    oagina

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { NewsApp } from "./NewsApp";
 import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <NewsApp />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <NewsApp />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- src/news/componets/NewsList.jsx
+++ src/news/componets/NewsList.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { startGetArticlesBySearch } from "../../store/news/thunks";
 
@@ -5,8 +6,12 @@ export const NewsList = () => {
   const dispatch = useDispatch();
   const { articles } = useSelector((state) => state.news);
 
-  dispatch(startGetArticlesBySearch());
+  useEffect(() => {
+    dispatch(startGetArticlesBySearch());
+  }, [articles])
 
+ 
+  
   return (
     <>
       <div className="row mb-2 d-flex justify-content-center">
